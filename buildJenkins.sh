e190b7da64532b5b6d39a0d4c2d832ff3e5b3c8d
rchaser53 <tayoshizawa29@gmail.com>
2017-07-31 17:37:22 +0900

    try to use token for security

--- buildJenkins.sh
+++ buildJenkins.sh
@@ -1,5 +1,6 @@
 USER='nya-n'
 PASSWORD='gya-n'
+WEBTOKEN='testtoken'
 
 TOKEN=`curl "http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)"  --user $USER:$PASSWORD`
-curl http://localhost:8080/job/ttt/build -X POST --user $USER:$PASSWORD -H $TOKEN
+curl http://localhost:8080/job/ttt/build?token=$WEBTOKEN -X POST --user $USER:$PASSWORD -H $TOKEN
