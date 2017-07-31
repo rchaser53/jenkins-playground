2247a9856a7d1a41aae648561d9117c0f3834325
rchaser53 <tayoshizawa29@gmail.com>
2017-07-31 17:29:10 +0900

    fix shell

--- buildJenkins.sh
+++ buildJenkins.sh
@@ -1,5 +1,5 @@
-USER = 'nya-n'
-PASSWORD = 'gya-n'
+USER='nya-n'
+PASSWORD='gya-n'
 
-TOKEN = curl "http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)"  --user $USER:$PASSWORD
-curl http://localhost:8080/job/testbuild/build -X POST --user $USER:$PASSWORD -H $TOKEN
+TOKEN=`curl "http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)"  --user $USER:$PASSWORD`
+curl http://localhost:8080/job/ttt/build -X POST --user $USER:$PASSWORD -H $TOKEN
